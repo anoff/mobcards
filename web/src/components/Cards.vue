dac50e0e45a2d55f5438cff0889116c64dd9a015
anoff <offenhaeuser@gmail.com>
2018-04-16 20:36:17 +0200

    :star2: feat: use cards

--- web/src/components/Cards.vue
+++ web/src/components/Cards.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
-    <md-steppers md-alternative>
-      <md-step v-for="card in cards" v-bind:key="$index" v-bind:md-label="card">
-        {{ card }}
-      </md-step>
-    </md-steppers>
+    <md-card v-for="card in cards" v-bind:key="card" >
+    <md-card-media-actions md-solid>
+      <md-card-media md-ratio="4:3">
+        <img src="/assets/examples/villains.png" alt="card">
+      </md-card-media>
+
+      <md-card-area>
+        <md-card-header>
+          <span class="md-title">{{ card }}</span>
+        </md-card-header>
+      </md-card-area>
+
+      <md-card-actions>
+        <md-button class="md-icon-button md-raised md-primary">
+          <md-icon>favorite</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button md-raised">
+          <md-icon>share</md-icon>
+        </md-button>
+      </md-card-actions>
+    </md-card-media-actions>
+  </md-card>
   </div>
 </template>
 
@@ -18,4 +36,11 @@ export default {
 </script>
 
 <style scoped>
+.md-card {
+  width: 320px;
+  height: 240px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
 </style>
