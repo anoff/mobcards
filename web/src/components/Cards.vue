f59e271ae25ed55e9ca99a840e3ab0598c082850
anoff <offenhaeuser@gmail.com>
2018-04-16 21:59:56 +0200

    :star2: feat: rendered text + button states

--- web/src/components/Cards.vue
+++ web/src/components/Cards.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-    <md-card v-for="card in cards" v-bind:key="card" >
+    <md-card v-for="card in cards" v-bind:key="card.id" >
     <md-card-media-actions md-solid>
-      <md-card-media md-ratio="4:3">
-        <img src="/assets/examples/villains.png" alt="card">
-      </md-card-media>
-
-      <md-card-area>
-        <md-card-header>
-          <span class="md-title">{{ card }}</span>
-        </md-card-header>
-      </md-card-area>
+        <md-card-area>
+          <md-card-content>
+            <span v-html="parseText(question, card.text)"></span>
+          </md-card-content>
+        </md-card-area>
 
       <md-card-actions>
-        <md-button class="md-icon-button md-raised md-primary">
+        <md-button class="md-icon-button md-raised md-primary" @click="selected = card.id" v-bind:class="{ 'md-accent': selected == card.id }">
           <md-icon>favorite</md-icon>
         </md-button>
 
@@ -30,17 +26,30 @@
 export default {
   name: 'Cards',
   data: () => ({
-    cards: ['funny', 'funnier', 'LOL']
-  })
+    cards: [{id: 0, text: 'funny'},
+    {id: 1, text: 'funnier'},
+    {id: 2, text: 'LOL'}],
+    question: "a clown with a red nose is _",
+    selected: null
+  }),
+  methods: {
+    parseText: (q, a) => q.replace('_', `<u><b>${a}</b></u>`)
+  }
 }
 </script>
 
 <style scoped>
 .md-card {
+  border-radius: 2em;
+  background-color: black;
   width: 320px;
   height: 240px;
   margin: 4px;
   display: inline-block;
   vertical-align: top;
+}
+.md-card-content {
+  color: white;
+  font-size: 1.8em;
 }
 </style>
