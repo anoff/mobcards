a144fe528dd681346e45b6f6220f8dbd42802532
anoff <offenhaeuser@gmail.com>
2018-04-19 23:41:32 +0200

    :star2: feat: start game via modal

---
+++ web/src/components/Host.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+  <md-dialog-prompt
+      :md-active.sync="active"
+      v-model="name"
+      md-title="Want to start a new lobby?"
+      md-input-maxlength="30"
+      md-input-placeholder="Type your name..."
+      md-confirm-text="Done"
+      md-cancel-text="Cancel"
+      @md-cancel="onCancel"
+      @md-confirm="onConfirm" />
+
+    <span v-if="name">name: {{ name }}</span>
+    <md-button class="md-fab md-primary" v-on:click="active = true">
+      <md-icon>add</md-icon>
+    </md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Host',
+  data: () => ({
+    active: false,
+    name: null
+  }),
+  methods: {
+    onConfirm () {
+      
+    },
+    onCancel () {
+      this.name = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Host from '@/components/Host'
 import Cards from '@/components/Cards'
 
 Vue.use(Router)
@@ -9,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Host',
+      component: Host
     },
     {
       path: '/cards',
