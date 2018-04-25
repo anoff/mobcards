15fd51cb4a22b2f8b56e4da9c1aa843c412e124d
anoff <offenhaeuser@gmail.com>
2018-04-25 22:30:18 +0200

    :construction_worker: build: run webpack post install

--- infra/main.tf
+++ infra/main.tf
@@ -21,4 +21,12 @@ resource "azurerm_app_service" "host" {
   location            = "${azurerm_resource_group.smartcards.location}"
   resource_group_name = "${azurerm_resource_group.smartcards.name}"
   app_service_plan_id = "${azurerm_app_service_plan.host.id}"
+
+  provisioner "local-exec" {
+    command = "az webapp deployment source config-local-git -g ${azurerm_resource_group.smartcards.name} -n ${azurerm_app_service.host.name}"
+  }
+}
+
+output "git_url" {
+  value = "${join("", list(azurerm_app_service.host.name, ".scm.azurewebsites.net:443/", azurerm_app_service.host.name, ".git"))}"
 }
