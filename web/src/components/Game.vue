512643fd70ff89c424ede906361a667b45c351ba
anoff <offenhaeuser@gmail.com>
2018-04-27 23:59:48 +0200

    :star2: feat: try vueswing for swipable cards

--- web/src/components/Game.vue
+++ web/src/components/Game.vue
@@ -1,24 +1,29 @@
 <template>
-  <div>
-    <md-card v-for="card in cards" v-bind:key="card.id" >
-    <md-card-media-actions md-solid>
-        <md-card-area>
-          <md-card-content>
-            <span v-html="parseText(question, card.text)"></span>
-          </md-card-content>
-        </md-card-area>
+  <div class="card-container">
+  <vue-swing
+    @throwout="throwout"
+    @throwin="throwin"
+  >
+    <md-card v-for="card in cards" v-bind:key="card.id" v-if="visibleCards.indexOf(card.id) > -1">
+      <md-card-media-actions md-solid>
+          <md-card-area>
+            <md-card-content>
+              <span v-html="parseText(question, card.text)"></span>
+            </md-card-content>
+          </md-card-area>
 
-      <md-card-actions>
-        <md-button class="md-icon-button md-raised md-primary" @click="selected = card.id" v-bind:class="{ 'md-accent': selected == card.id }">
-          <md-icon>favorite</md-icon>
-        </md-button>
+        <md-card-actions>
+          <md-button class="md-icon-button md-raised md-primary" @click="selected = card.id" v-bind:class="{ 'md-accent': selected == card.id }">
+            <md-icon>favorite</md-icon>
+          </md-button>
 
-        <md-button class="md-icon-button md-raised">
-          <md-icon>share</md-icon>
-        </md-button>
-      </md-card-actions>
-    </md-card-media-actions>
-  </md-card>
+          <md-button class="md-icon-button md-raised">
+            <md-icon>share</md-icon>
+          </md-button>
+        </md-card-actions>
+      </md-card-media-actions>
+    </md-card>
+  </vue-swing>
   </div>
 </template>
 
@@ -30,10 +35,26 @@ export default {
     {id: 1, text: 'funnier'},
     {id: 2, text: 'LOL'}],
     question: "a clown with a red nose is _",
-    selected: null
+    selected: null,
+    visibleCards: [0, 1]
   }),
   methods: {
-    parseText: (q, a) => q.replace('_', `<u><b>${a}</b></u>`)
+    parseText: (q, a) => q.replace('_', `<u><b>${a}</b></u>`),
+    throwout (data) {
+      this.visibleCards[0]++
+      this.visibleCards[1]++
+
+      if (this.visibleCards[0] > 2) {
+        this.visibleCards[0] = 0
+      }
+      if (this.visibleCards[1] > 2) {
+        this.visibleCards[1] = 0
+      }
+      console.log(this.visibleCards, data)
+    },
+    throwin (data) {
+      console.log(data, 'in')
+    }
   }
 }
 </script>
@@ -48,9 +69,15 @@ export default {
   margin: 4px;
   display: inline-block;
   vertical-align: top;
+  top: 0;
+  left: 0;
+  position: absolute;
 }
 .md-card-content {
   color: white;
   font-size: 2em;
+}
+.card-container {
+  position: relative;
 }
 </style>
--- web/src/main.js
+++ web/src/main.js
@@ -7,9 +7,13 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 import 'vue-material/dist/theme/black-green-light.css'
 import VueSocketIO from 'vue-socket.io'
+import VueSwing from 'vue-swing'
+import VueCarousel from 'vue-carousel'
 
 Vue.use(VueSocketIO, process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : window.location.origin)
 Vue.use(VueMaterial)
+Vue.component('vue-swing', VueSwing)
+Vue.use(VueCarousel)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
